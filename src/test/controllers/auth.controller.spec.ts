4c3605335790b5d87f49cfd37e18f4fae90f65b6
Caio Salgado Nepomuceno <csn.caio@gmail.com>
2023-02-23 20:32:44 -0300

    test: create User tests

--- src/test/controllers/auth.controller.spec.ts
+++ src/test/controllers/auth.controller.spec.ts
@@ -3,20 +3,22 @@ import request from 'supertest';
 import app from '../../app';
 import { registerUserDTO } from '../../controllers/dtos/register-user.dto';
 
+function createRegisterUserDTO(): registerUserDTO {
+  return {
+    name: faker.name.fullName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    role: 'NORMAL_USER',
+    dob: 'I don\'t know what it mean'
+  }
+}
+
 describe('Auth Controller Tests', () => {
   describe('Sign Up', () => {
-    let userDTO: registerUserDTO;
 
     describe('DTO Validation', () => {
-      beforeEach(() => {
-        userDTO = {
-          name: faker.name.fullName(),
-          email: faker.internet.email(),
-          password: faker.internet.password(),
-          role: 'NORMAL_USER',
-          dob: 'I don\'t know what it mean'
-        }
-      })
+      let userDTO: registerUserDTO;
+      beforeEach(() => userDTO = createRegisterUserDTO())
 
       it('Should return a BAD_REQUEST if e-mail isn\'t in the body', async () => {
         const user = { ...userDTO }
@@ -78,11 +80,37 @@ describe('Auth Controller Tests', () => {
         expect(response.body[0]).toStrictEqual({ isDefined: 'role should not be null or undefined' })
       })
 
-      it.todo('Should return a CREATED if dob isn\'t in the body')
+      it('Should return a CREATED if dob isn\'t in the body', async () => {
+        const user = { ...userDTO }
+        delete user.dob;
+
+        const response = await request(app)
+          .post('/api/register')
+          .set('Accept', 'application/json')
+          .send(user)
+
+        expect(response.headers["content-type"]).toMatch(/json/);
+        expect(response.status).toEqual(201);
+        expect(response.body.success).toBeTruthy()
+      })
     })
 
     describe('DB Validation', () => {
-      it.todo('Should return a CREATED and persist in DB if DTO is correct')
+      let userDTO: registerUserDTO;
+      beforeEach(() => userDTO = createRegisterUserDTO())
+
+      it('Should return a CREATED and persist in DB if DTO is correct', async () => {
+        const user = { ...userDTO }
+
+        const response = await request(app)
+          .post('/api/register')
+          .set('Accept', 'application/json')
+          .send(user)
+
+        expect(response.headers["content-type"]).toMatch(/json/);
+        expect(response.status).toEqual(201);
+        expect(response.body.success).toBeTruthy()
+      })
     })
 
   })
